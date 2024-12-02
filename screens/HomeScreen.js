9bca91a6b6ecdc3008e31e12f00e16fd90ba3815
Zimil-Patel <patelzimil2310@gmail.com>
2024-12-02 07:24:06 +0530

    02 Dec 2024

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { View, TextInput, StatusBar, Image, StyleSheet, TouchableOpacity, Text }
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const theme = {
   bgWhite: (opacity) => `rgba(255, 255, 255, ${opacity})`,
@@ -29,7 +30,7 @@ export default function HomeScreen({ navigation }) {
         style={homeStyles.fullScreenImage}
       />
 
-      <SafeAreaView style={{margin: 10, flex: 1, flexDirection :'column'}}>
+      <SafeAreaView style={{ marginVertical: 10, flex: 1, flexDirection: 'column', justifyContent: 'space-evenly' }}>
 
         {/* Search Section */}
         <View style={[homeStyles.searchSection]}>
@@ -87,10 +88,174 @@ export default function HomeScreen({ navigation }) {
 
         {/* Forecast Section */}
         <View style={[homeStyles.forecastColumn]}>
+
+
           {/* Location Text */}
           <Text style={[homeStyles.locationText]}>
-            London, United Kingdom
+            London,
+            <Text style={homeStyles.subLoationText}>
+              United Kingdom
+            </Text>
           </Text>
+
+
+
+            {/*weather image*/}
+            <View style={homeStyles.weatherImageContainer}>
+              <Image source={require('../assets/weatherImages/partlyCloudy.png') }
+                style = {homeStyles.weatherImage}
+              />
+            </View>
+
+
+            {/*degree celsius*/}
+            <View style = {{marginTop: 20, marginBottom:50, alignItems: 'center'}}>
+
+              <Text style = {[homeStyles.locationText, {fontSize: 50}]}>
+                23&#176;
+              </Text>
+
+              <Text style = {homeStyles.weatherConditionText}>
+                Partly Cloudy
+              </Text>
+
+            </View>
+
+
+            {/*other stats*/}
+            <View style={homeStyles.otherStat}> 
+
+              <View style={homeStyles.statImageLable}>
+
+                <Image source={require("../assets/statImages/wind.png")} style={homeStyles.statImage}/>
+
+                <Text style={homeStyles.statText}>22km</Text>
+
+              </View>
+              
+
+              <View style={homeStyles.statImageLable}>
+
+                <Image source={require("../assets/statImages/drop.png")} style={homeStyles.statImage}/>
+
+                <Text style={homeStyles.statText}>23%</Text>
+
+              </View>
+
+
+              <View style={homeStyles.statImageLable}>
+
+                <Image source={require("../assets/statImages/sun.png")} style={homeStyles.statImage}/>
+
+                <Text style={homeStyles.statText}>6:05 AM</Text>
+
+              </View>
+
+            </View>
+
+            {/*forecast section for next coming  days*/}
+            <View style={homeStyles.nextDayForecastSection} >
+              
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginLeft: 20}}>
+
+                <FontAwesome name='calendar' color={'lightgray'} size={18}/>
+
+                <Text style={[homeStyles.statText, {fontSize: 16}]}>
+                  Daily Forecast
+                </Text>
+
+              </View>
+
+
+              {/*horizontal forecast scroll view*/}
+              <ScrollView
+                horizontal
+                contentContainerStyle = {{marginHorizontal: 15}}
+                showsHorizontalScrollIndicator = {false}
+              >
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+                <View style={[homeStyles.nextForecastBox, {backgroundColor: theme.bgWhite(0.15)}]} >
+
+                  <Image source={require("../assets/weatherImages/partlyCloudy.png")} style = {homeStyles.nextForecastImage} />
+
+                  <Text style={homeStyles.statText}>Monday</Text>
+
+
+                  <Text style={[homeStyles.statText, {fontWeight: 'bold'}]} >23&#176;</Text>
+
+                </View>
+
+              </ScrollView>
+
+
+            </View>
+
         </View>
 
       </SafeAreaView>
@@ -111,8 +276,9 @@ const homeStyles = StyleSheet.create({
     position: 'absolute',
   },
 
-  searchSection: {  
-    height: '7%', 
+  searchSection: {
+    marginHorizontal: 10,
+    height: '7%',
     zIndex: 2, // Ensures it stays on top
   },
 
@@ -136,7 +302,7 @@ const homeStyles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50,
-    
+
   },
   searchResult: {
     position: 'absolute',
@@ -164,18 +330,97 @@ const homeStyles = StyleSheet.create({
   forecastColumn: {
     flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
-    marginHorizontal: 4,
-    justifyContent: 'center',
-    top: 60,
+    justifyContent: 'space-between',
+    top: 20,
     marginBottom: 4,
     width: '100%',
   },
 
   locationText: {
-    fontWeight: 'bold',
     textAlign: 'center',
+    fontWeight: 'bold',
     fontSize: 30,
     color: 'white',
+  },
+
+  subLoationText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'lightgray',
+    paddingLeft: 8,
+  },
+
+  weatherImageContainer: {
+    marginVertical: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  weatherImage: {
+    resizeMode: 'cover',
+    height: 200,
+    width: 200,
+  },
+
+  weatherConditionText: {
+    fontSize: 20,
+    color: 'white',
+    letterSpacing: 2,
+    textAlign: 'center',
+  },
+
+  otherStat: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+
+  statImageLable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+  },
+
+  statImage: {
+    height: 28,
+    width: 28,
+    resizeMode: 'contain',
+    tintColor: 'lightgray',
+  },
+
+  statText: {
+    fontSize: 17,
+    marginHorizontal: 10,
+    color: 'lightgray',
+    letterSpacing: 2,
+    textAlign: 'center'
+  },
+
+  nextDayForecastSection: {
+    flexDirection: 'column',
+    marginTop: 20,
+    marginBottom: 40,
+    alignItems: 'flex-start',
+  },
+
+  nextForecastBox: {
+    flex: 'column',
+    height: 120,
+    justifyContent: 'space-evenly',
+    paddingVertical: 8,
+    alignItems: 'center',
+    marginRight: 15,
+    borderRadius: 20,
+    marginVertical: 15,
+  },
+
+  nextForecastImage: {
+    height: 38,
+    width: 38,
+    resizeMode: 'contain',
   }
 });
